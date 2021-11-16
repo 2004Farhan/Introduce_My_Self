58b8ff72f7b51974e38e60c5f04725fe634ed144
Farhan Firmansyah <94467464+2004Farhan@users.noreply.github.com>
2021-11-16 23:18:58 +0700

    Add files via upload

---
+++ 17Farhan.py
@@ -0,0 +1,10 @@
+Nama = 'Farhan Firmansyah'
+NIM = 212410102004
+Asal = 'Bondowoso'
+Jurusan = 'Teknologi Informasi'
+Fakultas = 'Ilmu Komputer'
+print('===========================================')
+print('   Perkenalkan Namaku', Nama,'\n''            NIM', NIM,'\n' 
+    '  Dari program studi', Jurusan,'\n''Fakultas', Fakultas,
+    'Universitas Jember''\n' '           Kota Asal', Asal)
+print("===========================================")
