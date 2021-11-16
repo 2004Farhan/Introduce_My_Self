feed3d045f694c24d836e639fb8d18818792182a
Farhan Firmansyah <94467464+2004Farhan@users.noreply.github.com>
2021-11-17 00:35:21 +0700

    Update 17Farhan.py

--- 17Farhan.py
+++ 17Farhan.py
@@ -1,10 +1,11 @@
 Nama = 'Farhan Firmansyah'
 NIM = 212410102004
 Asal = 'Bondowoso'
+Hobby = 'Fotografi'
 Jurusan = 'Teknologi Informasi'
 Fakultas = 'Ilmu Komputer'
 print('===========================================')
-print('   Perkenalkan Namaku', Nama,'\n''            NIM', NIM,'\n' 
-    '  Dari program studi', Jurusan,'\n''Fakultas', Fakultas,
+print('   Perkenalkan Namaku', Nama,'\n''            NIM', NIM,'\n'
+    '            Hobby', Hobby,'\n''  Dari program studi', Jurusan,'\n''Fakultas', Fakultas,
     'Universitas Jember''\n' '           Kota Asal', Asal)
 print("===========================================")
